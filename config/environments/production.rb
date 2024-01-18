dd69462d48a09a8564a1f5b15330b7580a85dc86
Josiah Smith <jcsmithf22@gmail.com>
2024-01-18 15:44:24 -0600

    specify host url for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :resend
+  config.action_mailer.default_url_options = { host: 'https://docgen-0rmn.onrender.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
