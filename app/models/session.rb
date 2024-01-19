6d546ec38895fca5308dabb76fc3c0a1bf27e79e
Josiah Smith <jcsmithf22@gmail.com>
2024-01-19 15:42:11 -0600

    attempt to fix strict loading error

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session < ApplicationRecord
-  belongs_to :user
-
+  belongs_to :user, strict_loading: false
+ 
   before_create do
     self.user_agent = Current.user_agent
     self.ip_address = Current.ip_address
