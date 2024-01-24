f0291e2b2fab6e8b3bf2a94a46343271f6db9db0
Josiah Smith <jcsmithf22@gmail.com>
2024-01-24 13:23:35 -0600

    fix importmap imports

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-dropdown" # @2.1.0
