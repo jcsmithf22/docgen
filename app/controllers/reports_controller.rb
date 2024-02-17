63740e0d2c177f57252f8127df53864477fedf5e
Josiah Smith <jcsmithf22@gmail.com>
2024-02-17 10:45:33 -0600

    add report delete

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,4 +5,16 @@ class ReportsController < ApplicationController
 
     redirect_to report_build_path(report_id: @report.id, id: :general)
   end
+
+  def destroy
+    # first delete all actions for the report
+    actions = Action.includes(:action_memberships).where(report_id: params[:id])
+    actions.destroy_all
+
+    report = Report.includes(:symptoms, :report_memberships, :attachments, :actions, root_causes: [:actions, :attachments]).find(params[:id])
+    # report = Report.find(params[:id])
+    report.destroy
+
+    redirect_to root_path, notice: 'Report has been deleted'
+  end
 end
